2727ba9d15f7b5b44e41c8b54584e75af6349321
lildesert <geonosis8@hotmail.com>
2015-11-04 16:50:43 +0100

    first commit

---
+++ Budjay/Gulpfile.js
@@ -0,0 +1,17 @@
+﻿var gulp = require('gulp');
+var mainBowerFiles = require('gulp-main-bower-files');
+var inject = require('gulp-inject');
+
+gulp.task('main-bower-files', function() {
+    return gulp.src('./bower.json')
+        .pipe(mainBowerFiles())
+        .pipe(gulp.dest('./www/libs'));
+});
+
+gulp.task('InjectFiles', function () {
+    var target = gulp.src('./www/index.html'); 
+    var sources = gulp.src(['./www/libs/**/*.js', './www/libs/**/*.css'], { read: false });
+
+    return target.pipe(inject(sources, { relative: true }))
+      .pipe(gulp.dest('./www'));
+});
